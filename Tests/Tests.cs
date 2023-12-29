ae612742fb0fcb5f36f23aa927b929c84407e8a3
avinashbhujan <avinashbhujan@gmail.com>
2023-12-29 16:21:13 +0400

    DB Script and unit test

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using MusicArtist.Controllers;
+using NUnit.Framework;
 using System.Threading.Tasks;
 
 namespace Tests
@@ -6,6 +7,17 @@ namespace Tests
     [TestFixture]
     public class Tests
     {
-        
+        [Test]
+        public async Task DoesFetchReleaseGroupsCountReturn25ForMetallica()
+        {
+            // Arrange
+            var controller = new ArtistController();
+
+            // Act
+            var result = await controller.FetchReleaseGroups("65f4f0c5-ef9e-490c-aee3-909e7ae6b2ab");
+
+            // Assert
+            Assert.That(result.Count, Is.EqualTo(25));
+        }
     }
 }
